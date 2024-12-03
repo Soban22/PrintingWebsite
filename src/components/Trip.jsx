f903cc8b2ab22bc1e76245212858e3804b78879d
Soban khan <rsoban22@gmail.com>
2024-12-04 00:54:32 +0500

    some main changes

--- src/components/Trip.jsx
+++ src/components/Trip.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
 import './trip.css'
 import TripData from './TripData'
-import Trip1 from '../assets/5.jpg'
-import Trip2 from '../assets/6.jpg'
-import Trip3 from '../assets/8.jpg'
+import Trip1 from '../assets/101.webp'
+import Trip2 from '../assets/102.webp'
+import Trip3 from '../assets/100.jpg'
 
 const Trip = () => {
   return (
     <div className='trip'>
-        <h1 className='mainheading'>Recent Trips</h1>
-        <p className='trippara'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, excepturi? Non harum officia id blanditiis ipsum inventore.</p>
+        <h1 className='mainheading'>Our Products and Services</h1>
+        <p className='trippara'>Our products and services are designed to simplify your financial journey, offering innovative solutions tailored to your needs. From cutting-edge tools to expert support, we empower you to achieve your goals effortlessly.</p>
         <div className='tripcard'>
             <TripData
             image={Trip1}
-            heading="product 1"
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, excepturi? Non harum officia id blanditiis ipsum inventore."
+            heading="Business Cards"
+            text="Professionally designed business cards that leave a lasting impression."
             />
             <TripData
             image={Trip2}
-            heading="product 2"
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, excepturi? Non harum officia id blanditiis ipsum inventore."
+            heading="Office Stationary"
+            text="High-quality office supplies customized to suit your brand identity."
             />
             <TripData
             image={Trip3}
-            heading="product 3"
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, excepturi? Non harum officia id blanditiis ipsum inventore."
+            heading="Product Photography"
+            text="Stunning visuals that showcase your products in the best light."
             />
             
         </div>
