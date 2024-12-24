0383258ad3037695ec8bf2c72b67fb45d8b6abc0
Soban khan <rsoban22@gmail.com>
2024-12-24 21:45:22 +0500

    necessary changes

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,15 +4,28 @@ import './contactform.css'
 const ContactForm = () => {
   return (
     <div className='form-container'>
+
+    <div className="contact-section">
+      <div className="contact-card">
+        <div className="icon-container">
+          <i className="phone-icon"></i>
+        </div>
+        <h3>Talk to us</h3>
+        <p>If you want to work with us, just let us know through this contact.</p>
+        <p className="contact-info">+92 321 7420007</p>
+      </div>
+      
+      <div className="contact-card">
+        <div className="icon-container">
+          <i className="location-icon"></i>
+        </div>
+        <h3>Address Information</h3>
+        <p><strong>Email us at:</strong><br/>info@farooqgraphics.com</p>
+        <p><strong>Office:</strong><br/>4-Ahmad pura, Gondlanwala Rd, near Jamia Masjid Tuba, Mohalla Noor Bawa, Gujranwala, Punjab</p>
+      </div>
+    </div>
        
-        <form action="">
-            <h1>Share Your Remarks!</h1>
-            <input type="text" placeholder='Name' />
-            <input type="email" placeholder='Email' />
-            <input type="text" placeholder='Subject' />
-            <textarea name="" placeholder='Message' rows="4" id=""></textarea>
-            <button>Send Message</button>
-        </form>
+       
         <div className='location'>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3377.4135871689723!2d74.1820542!3d32.1661202!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391f298798720773%3A0xbba4aaaf3f12ec26!2sUmar%20Printing%20Point!5e0!3m2!1sen!2s!4v1732895866464!5m2!1sen!2s" width="800" height="450"  allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
